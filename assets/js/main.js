a9e58959364a9d777e485fe5971b5a2d6d59c5b1
Ashish Sondagar <ashishsondagar@Ashishs-MacBook-Pro.local>
2024-06-13 14:10:31 +0530

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /*
 	Miniport by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	abc321.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (abc321.net/license)
 */
 
 (function($) {
